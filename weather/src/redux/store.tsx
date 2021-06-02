63e8eccbf1f1a9d4211538f9a3610a2568935bfb
isko88 <ismayilj@code.edu.az>
2021-06-02 08:17:05 +0400

    store ts added

--- weather/src/redux/store.tsx
+++ weather/src/redux/store.tsx
@@ -10,7 +10,6 @@ const rootReducer = combineReducers({
 
 const middlewares = [
     thunk,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ]
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
